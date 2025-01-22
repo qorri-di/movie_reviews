a0672cda1f1dc02804778a0f6a67f821ebca704d
Mr Esbatu <qorri24j@gmail.com>
2025-01-23 05:56:35 +0700

    add loading for login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:movie_reviews/widgets/custom_loading.dart';
+
 import '../api_service.dart';
 import 'movie_reviews_screen.dart';
 import 'register_screen.dart';
 
 class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
@@ -14,21 +18,32 @@ class _LoginScreenState extends State<LoginScreen> {
   final _apiService = ApiService();
 
   void _login() async {
+    // Show loading
+    CustomLoading.show();
+
     final success = await _apiService.loginUser(
       _usernameController.text,
       _passwordController.text,
     );
 
     if (success) {
+      // Dismiss loading
+      CustomLoading.dismiss();
+
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
-          builder: (context) => MovieReviewsScreen(username: _usernameController.text),
+          builder: (context) =>
+              MovieReviewsScreen(username: _usernameController.text),
         ),
       );
     } else {
+      // Dismiss loading
+      CustomLoading.dismiss();
+
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Login gagal. Silakan cek username/password.')),
+        const SnackBar(
+            content: Text('Login gagal. Silakan cek username/password.')),
       );
     }
   }
@@ -36,31 +51,31 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
+      appBar: AppBar(title: const Text('Login')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
             TextField(
               controller: _usernameController,
-              decoration: InputDecoration(labelText: 'Username'),
+              decoration: const InputDecoration(labelText: 'Username'),
             ),
             TextField(
               controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
+              decoration: const InputDecoration(labelText: 'Password'),
               obscureText: true,
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: _login,
-              child: Text('Login'),
+              child: const Text('Login'),
             ),
             TextButton(
               onPressed: () => Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => RegisterScreen()),
+                MaterialPageRoute(builder: (context) => const RegisterScreen()),
               ),
-              child: Text('Belum punya akun? Daftar di sini.'),
+              child: const Text('Belum punya akun? Daftar di sini.'),
             ),
           ],
         ),
